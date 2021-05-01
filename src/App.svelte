250452a5e84a7d3675e0851276a818d5a48f1679
nnathane <80011935+nnathane@users.noreply.github.com>
2021-05-01 06:57:35 -0400

    testing web app with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,35 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="card">
+    <h1>Breach was here LOL</h1>
+  </div>
+  <div class="card2">
+    <h1>LETS GOOOOOOOOO</h1>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .card {
+    text-align: center;
+    background-color: purple;
+    padding: 8px;
+    border-radius: 20px;
+    border: 2px solid darksalmon;
+  }
+  .card2 {
+    margin-top: 8px;
+    text-align: center;
+    background-color: orange;
+    padding: 8px;
+    border-radius: 20px;
+    border: 2px solid blue;
+  }
+  h1 {
+    color: crimson;
+  }
+  .card2 h1 {
+    color: black;
+  }
 </style>
